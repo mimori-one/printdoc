c4d52c2012e37f3d86da249e6a5501d64a07483c
mimori-one <mimori@onesheen.jp>
2023-12-27 10:58:48 +0900

    サンプル切り出し機能追加

---
+++ boards/_nuxt/DocumentDrivenNotFound.8931bfd7.js
@@ -0,0 +1,1 @@
+import{f as n,s as e}from"./entry.c1fe3a26.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
---
+++ boards/_nuxt/Markdown.eff4f434.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.881a44ad.js";import{f as o,m as f,J as u,D as m}from"./entry.c1fe3a26.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=m(),{between:n,default:a}=f(),s=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
